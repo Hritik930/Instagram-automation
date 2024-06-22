2ab5af2548d56a447be63bca50128888aea1f72e
Hritik <hritikblcm@gmail.com>
2024-06-23 01:04:02 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,6 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username = "@hk.001111",password="Nikita#@213")
+# bot.follow('tutorials_freak')
+
+bot.upload_photo("C:/Users\DELL\Pictures\Screenshots\s.png",caption="I learn Python")
